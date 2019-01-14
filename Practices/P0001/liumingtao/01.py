0c4b56b16fa25e2bfa137b945361fdd669f5c0fd
liumingtao412 <liumingtao412@hotmail.com>
2019-01-14 21:44:04 +0800

    完成01

---
+++ Practices/P0001/liumingtao/01.py
@@ -0,0 +1,10 @@
+import random
+lis = [random.randint(0, 100) for i in range(20)]
+def sortport(lis):
+    for i in range(len(lis) - 1):
+        for j in range(len(lis) - 1 - i):
+            if lis[j] > lis[j + 1]:
+                lis[j], lis[j + 1] = lis[j + 1], lis[j]
+print(lis)
+sortport(lis)
+print(lis)
